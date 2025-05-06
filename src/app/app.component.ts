809b0ad671f33e8035f647c954e364630fb10e69
Ana Caroll <ana2417carollina@gmail.com>
2025-05-06 17:30:50 -0300

    calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'calc';
+  display = '';
+  theme = 1;
+
+  @HostBinding('class')
+  get themeClass() {
+    return `theme-${this.theme}`;
+  }
+
+  append(value: string) {
+    this.display += value;
+  }
+
+  delete() {
+    this.display = this.display.slice(0, -1);
+  }
+
+  reset() {
+    this.display = '';
+  }
+
+  calculate() {
+    try {
+      const result = eval(this.display.replace(/x/g, '*'));
+      this.display = Number(result).toLocaleString();
+    } catch {
+      this.display = 'Error';
+    }
+  }
+
+  switchTheme() {
+    this.theme = this.theme < 3 ? this.theme + 1 : 1;
+  }
 }
