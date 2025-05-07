e7734c100193587e0e89fcbb51997dd0a2cc7d35
Ana Caroll <ana2417carollina@gmail.com>
2025-05-07 14:58:37 -0300

    add spec component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,74 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [CommonModule],
+      declarations: [AppComponent],
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'calc' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('calc');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calc');
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should append value to display', () => {
+    component.append('123');
+    component.append('4');
+    expect(component.display).toBe('1234');
+  });
+
+  it('should delete last caracter from display', () => {
+    component.append('1234');
+    component.delete();
+    expect(component.display).toBe('123');
+  });
+
+  it('should reset display', () => {
+    component.append('1234');
+    component.reset();
+    expect(component.display).toBe('');
+  });
+
+  it('should calculate valid expression', () => {
+    component.display = '2+2';
+    component.calculate();
+    expect(component.display).toBe('4');
+  });
+
+  it('should handle multiplication using x', () => {
+    component.display = '2x2';
+    component.calculate();
+    expect(component.display).toBe('4');
+  });
+
+  it('should handle division using /', () => {
+    component.display = '2/2';
+    component.calculate();
+    expect(component.display).toBe('1');
+  });
+
+  it('should display error message for invalid expression', () => {
+    component.display = '2+';
+    component.calculate();
+    expect(component.display).toBe('Error');
+  });
+
+  it('should switch theme from 1 to 2, 3, the back to 1', () => {
+    component.switchTheme();
+    expect(component.theme).toBe(2);
+    component.switchTheme();
+    expect(component.theme).toBe(3);
+    component.switchTheme();
+    expect(component.theme).toBe(1);
   });
 });
